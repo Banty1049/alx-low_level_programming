9b9b8418c5d0ac317ac01a3f7a23722816335930
root <akinseindetbanty@gmail.com>
2023-02-16 11:09:34 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf fu
+ * nction
+ * Return : 0 (success)
+ */
+{
+	printf('With proper grammer, but the outcome is a piece
+			of art,\n")
+		return (0);
+}
+
